0a472eaf07565a544127332c7c57a4c5e3e76edd
T. Ehrler <30443689+TMelchiorE@users.noreply.github.com>
2023-05-05 13:21:13 -0400

    Create example_workflow.js

---
+++ example_workflow.js
@@ -0,0 +1,53 @@
+const crypto = require('crypto');
+const { Octokit } = require("@octokit/core");
+const sodium = require('libsodium-wrappers');
+
+const octokit = new Octokit({ auth: GHE_PAT });
+
+const secret = "ghfgghfJJJDDD" // just testing - will come from env
+  
+encryptUpdateRepoSecret()
+      .then(res => {
+        console.log({ res })
+        process.exitCode = 0
+      })
+      .catch(err => {
+        console.log({ err })
+        process.exitCode = 1
+      })
+ 
+async function encryptUpdateRepoSecret() {
+  
+  const publicKeyResponse = await octokit.request('GET /repos/TMelchiorE/testing-actions/actions/secrets/public-key', {
+    owner: 'TMelchiorE',
+    repo: 'testing-actions',
+    })
+
+  const publicKey = publicKeyResponse.data.key;
+  const publicKeyId = publicKeyResponse.data.key_id;
+
+  console.log("--repo pubkey=" + publicKey)
+
+  // Convert Secret & Base64 key to Uint8Array.
+  let binkey = sodium.from_base64(publicKey, sodium.base64_variants.ORIGINAL)
+  let binsec = sodium.from_string(secret)
+  
+  //Encrypt the secret using LibSodium
+  let encBytes = sodium.crypto_box_seal(binsec, binkey)
+  
+  // Convert encrypted Uint8Array to Base64
+  let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)
+  
+  console.log("--encrpyted secret=" + output)
+
+  const updateSecretResponse = await octokit.request('PUT /repos/TMelchiorE/testing-actions/actions/secrets/JUST_TESTING', {
+    owner: 'TMelchiorE',
+    repo: 'testing-actions',
+    secret_name: 'JUST_TESTING',
+    key_id: publicKeyId,
+    encrypted_value: output,
+  });
+
+  console.log (updateSecretResponse)
+
+};
